baa6d658df4e335e227847951a5309e042e7cc0d
valen96 <valentin@breik.app>
2023-05-30 11:02:02 -0400

    fix path

--- composables/useMyFetch.js
+++ composables/useMyFetch.js
@@ -2,7 +2,7 @@ export const useMyFetch = async (request, opts) => {
   const config = useRuntimeConfig();
 
   return await useFetch(request, {
-    baseURL: `${config.public.baseURL}infojobs/`,
+    baseURL: `${config.public.baseURL}/infojobs/`,
     ...opts,
   });
 };
--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
-      baseURL: process.env.BASE_URL || "http://localhost:8000/",
+      baseURL: process.env.BASE_URL || "http://localhost:8000",
     },
   },
   modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
